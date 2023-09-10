99acf86f972d352aa7da370fbaf1cd05e6a3156f
Lucas Cicala Pena <79058932+LucasCPena@users.noreply.github.com>
2023-09-10 12:17:29 -0300

    Validação de login adicionada

---
+++ login.js
@@ -0,0 +1,29 @@
+document.getElementById("btnentrar").addEventListener("click", function() {
+    var usuario = document.getElementById("usuario").querySelector("input").value;
+    var senha = document.getElementById("senha").querySelector("input").value;
+
+    if (usuario === "" || senha === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    var usuariosValidos = [
+        { email: "teste@gmail.com", senha: "teste" },
+       
+    ];
+
+    // Verificando se o usuário e senha correspondem a um usuário válido
+    var usuarioValido = usuariosValidos.find
+    (function(usuar){
+        
+        return usuar.email === usuario && usuar.senha === senha;
+    });
+
+    if (usuarioValido) {
+        alert("Login bem-sucedido!");
+        // Rediciona para a pagina main
+         window.location.href = "Main.html";
+    } else {
+        alert("Credenciais inválidas. Por favor, tente novamente.");
+    }
+});
