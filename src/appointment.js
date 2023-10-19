df0c0d518090bc31162a17e3d58377568cc9cb50
matheusAFreitas <matheus.freitas@fcamara.com.br>
2023-10-19 01:23:46 -0300

    feat: added createAppointment integration

--- src/appointment.js
+++ src/appointment.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var reservations = [];
+document.addEventListener("DOMContentLoaded", function () {
+  var reservations = [];
 
-    flatpickr("#date", {
-        dateFormat: "Y-m-d",
-        minDate: "today",
-        locale: 'pt'
-    });
+  flatpickr("#date", {
+    dateFormat: "Y-m-d",
+    minDate: "today",
+    locale: "pt",
+  });
 
-    function displayReservations() {
-        var reservationList = document.getElementById('reservation-list');
-        reservationList.innerHTML = '';
+  function displayReservations() {
+    var reservationList = document.getElementById("reservation-list");
+    reservationList.innerHTML = "";
 
-        for (var i = 0; i < reservations.length; i++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = "Sala reservada " + reservations[i].start;
-            reservationList.appendChild(listItem);
-        }
+    for (var i = 0; i < reservations.length; i++) {
+      var listItem = document.createElement("li");
+      listItem.textContent = "Sala reservada " + reservations[i].start;
+      reservationList.appendChild(listItem);
     }
+  }
 
-    function addReservation() {
-        var selectedDate = document.getElementById('date').value;
+  function addReservation() {
+    var selectedDate = document.getElementById("date").value;
 
-        if (selectedDate) {
-            reservations.push({ start: selectedDate });
-            displayReservations();
-            alert('Reserva cadastrada com sucesso!');
-        }
+    if (selectedDate) {
+      reservations.push({ start: selectedDate });
+      displayReservations();
+      alert("Reserva cadastrada com sucesso!");
     }
+  }
 
-    document.querySelector('.search-container button').addEventListener('click', addReservation);
+  document
+    .querySelector(".search-container button")
+    .addEventListener("click", addReservation);
 });
