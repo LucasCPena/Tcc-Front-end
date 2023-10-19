edfa77ce7256c168be07e5f1420aa431d0f0e44a
matheusAFreitas <matheus.freitas@fcamara.com.br>
2023-10-18 22:41:42 -0300

    feat: added login integration

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const path = require('path')
-const cors = require('cors')
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 const router = express.Router();
 const port = 5000;
-const filepath = path.join(__dirname,'front/login.html')
 
-app.use(cors())
-router.get('/login', (req, res) => res.sendFile(filepath));
+const login = path.join(__dirname, "src/login.html");
+//const main = path.join(__dirname, "src/Main.html");
 
-app.use("/",express.static("front"))
+app.use(cors());
+app.use(express.static("node_modules"));
+router.get("/login", (req, res) => res.sendFile(login));
+//router.get("/main", (req, res) => res.sendFile(main));
 
-app.use(router)
+app.use("/", express.static("src"));
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Servidorrodando na porta ${port}`);
